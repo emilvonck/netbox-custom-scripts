6f94306e2462e1c1ab1246de230fe7ce03f2da23
Emil Vonck <emil.vonck@gmail.com>
2024-10-01 20:35:14 +0200

    Add custom script to add tenants

---
+++ create_tenant.py
@@ -0,0 +1,21 @@
+from tenancy.models import Tenant
+from extras.scripts import Script, StringVar
+from django.utils.text import slugify
+
+class CreateTenant(Script):
+    class Meta:
+        name = "Create Tenant"
+        description = "My demo script"
+
+
+    tenant_name = StringVar(label="Tenant Name")
+    tenant_desc = StringVar(label="Tenant Desc")
+
+    def run(self, data, commit):
+        tenant = Tenant(
+            name=data['tenant_name'],
+            slug=slugify(data['tenant_name']),
+            description=data['tenant_desc']
+        )
+        tenant.save()
+        self.log_success(f"Created new tenant: {tenant}")
