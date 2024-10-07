f391abd3aae1c45cbf7f1aa78a5830e221675365
Emil Vonck <emil.vonck@gmail.com>
2024-10-07 15:41:17 +0200

    Add gaming console

---
+++ create_gaming_console.py
@@ -0,0 +1,24 @@
+from extras.scripts import *
+from django.utils.text import slugify
+from dcim.choices import DeviceStatusChoices, SiteStatusChoices
+from dcim.models import Device, DeviceRole, DeviceType, Site
+from ipam.models import Prefix, IPAddress, IPRange
+
+class CreateSite(Script):
+    class Meta: # type: ignore
+        name = "Create Gaming Console"
+        description = "Script to create a new gaming console"
+
+    site_input = ObjectVar(
+        description="Site name",
+        model=Site
+    )
+
+    def run(self, data, commit):
+        site = Site.objects.get(name=data['site_input'].name)
+        self.log_success(site)
+
+        # Create devices
+        # self.log_success(dir(Prefix))
+        # self.log_success(dir(IPAddress))
+        # self.log_success(dir(IPRange))
